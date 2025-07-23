87cbff4d3539a40f2040efb2acc00b85029f74ac
yezerpo <kalidium@outlook.com>
2025-07-23 23:02:54 +0800

    添加favicon图标到VuePress配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,11 +4,14 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  head: [
+    [
+      'link', { rel: 'icon', href: 'favicon.ico' }
+    ]
+  ],
   lang: "zh-CN",
   title: "",
   description: "",
-
   theme,
 
 });
